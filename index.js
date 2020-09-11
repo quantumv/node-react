95143fcf70f64f0e65d925b24a17c4eacf79c339
quantum.v.rl <quantum.v.rl@gmail.com>
2020-09-10 20:22:50 -0700

    oauth

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Root file --Imports--
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const keys = require('./config/keys')
+
 const app = express()
 
 // route
-app.get('/', (req, res) => {
-    res.send({ hi: 'Q' })
-})
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+        console.log(accessToken)
+        }
+    )
+)
 
 //listener
 const PORT = process.env.PORT || 5000
