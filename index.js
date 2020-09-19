ea54509a13f99c63e70f9424cd064cfe1d55a231
quantum.v.rl <quantum.v.rl@gmail.com>
2020-09-18 21:51:45 -0700

    routes, services

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-// Root file --Imports--
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('./config/keys')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-// route
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        accessToken => {
-        console.log(accessToken)
-        }
-    )
-)
+require('./routes/authRoutes')(app)
 
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
-//listener
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
-console.log('App open')
-
