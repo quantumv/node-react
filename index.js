92437d6a2ca7348425d47c678082baefdd16abf1
quantum.v.rl <quantum.v.rl@gmail.com>
2020-09-19 14:59:17 -0700

    models fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
-require('./models/User.js')
+require('./models/User')
 require('./services/passport')
 
 
--- services/passport.js
+++ services/passport.js
@@ -14,13 +14,13 @@ passport.use(
         },
         (accessToken,refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then((existingUser) => {
-                    if (existingUser) {
-                        // we already have a record with given profile ID
-                    } else {
-                        // we don't have a user record with this ID, make a new record
-                        new User({ googleId: profile.id }).save()
-                    }
-                })
+                if (existingUser) {
+                    // we already have a record with given profile ID
+                } else {
+                    // we don't have a user record with this ID, make a new record
+                    new User({ googleId: profile.id }).save()
+                }
+            })
         }
     )
 )
