8ceb75790eba02499128235b7967810dbaf275f9
quantum.v.rl <quantum.v.rl@gmail.com>
2020-09-23 16:57:39 -0700

    i cookies session

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user)
+    })
+})
+
 passport.use(
     new GoogleStrategy(
         {
@@ -13,12 +24,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken,refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
+            User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
                 if (existingUser) {
                     // we already have a record with given profile ID
+                    done(null, existingUser)
                 } else {
                     // we don't have a user record with this ID, make a new record
-                    new User({ googleId: profile.id }).save()
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user))
                 }
             })
         }
