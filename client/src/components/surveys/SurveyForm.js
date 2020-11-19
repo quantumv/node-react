ba7ca24bac95538083429afc9289362a5e2afb1e
quantum.v.rl <quantum.v.rl@gmail.com>
2020-11-18 18:15:34 -0800

    surveyFormReview

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -31,9 +31,9 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
-                    <Link to="surveys" className="red btn-flat white-text">
+                    <Link to="/surveys" className="red btn-flat white-text">
                         Cancel
                     </Link>
                     <button type="submit" className="teal btn-flat right white-text">
@@ -48,9 +48,7 @@ class SurveyForm extends Component {
 
 function validate(values) {
     const errors = {};
-
-    errors.emails = validEmails(values.emails || '');
-
+    
     _.each(FIELDS, ({ name }) => {
         if (!values[name]) {
             errors[name] = 'You must provide a value';
@@ -58,11 +56,12 @@ function validate(values) {
     });
 
     
-
+    errors.emails = validEmails(values.emails || '');
     return errors;
 }
 
 export default reduxForm({
     validate: validate,
-    form: 'surveyForm'
+    form: 'surveyForm',
+    destroyOnUnmount: false
 }) (SurveyForm);
